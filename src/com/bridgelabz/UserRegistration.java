a13503fed9822e495f3e4c203625066fb482e8f5
Ashwini Rathod <rashwini510@gmail.com>
2022-06-30 12:58:45 +0530

    Added: Ability to check user's first name valid or not

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to user registration program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
     }
 }
