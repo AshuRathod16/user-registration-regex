54e6153bcd8fd3a20cf7d3efee0a9ccae305a15f
Ashwini Rathod <rashwini510@gmail.com>
2022-06-30 20:52:36 +0530

    Added: Ability to check the all valid or invalid email samples

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,35 +1,25 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to user registration program");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter First Name: ");
-        String firstName = scanner.nextLine();
-        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
-        System.out.println("Enter Last Name: ");
-        String lastName = scanner.nextLine();
-        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
-        System.out.println("Enter E-mail: ");
-        String email = scanner.nextLine();
-        System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,5}", email));
-        System.out.println("Enter Mobile Number: ");
-        String mobileNumber = scanner.nextLine();
-        System.out.println(Pattern.matches("[+]91 [6-9]\\d{9}", mobileNumber));
-        System.out.println("Enter Password1: ");
-        String password1 = scanner.nextLine();
-        System.out.println(Pattern.matches("[a-z A-Z]{8,}", password1));
-        System.out.println("Enter Password2: ");
-        String password2 = scanner.nextLine();
-        System.out.println(Pattern.matches("[A-Z][a-z A-Z]{8,}", password2));
-        System.out.println("Enter Password3: ");
-        String password3 = scanner.nextLine();
-        System.out.println(Pattern.matches("[A-Z][a-z A-Z 0-9]{8,}", password3));
-        System.out.println("Enter Password4: ");
-        String password4 = scanner.nextLine();
-        System.out.println(Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[$&+,:;=?@#|'<>.-^*()%!])[a-z A-Z \\d $&+,:;=?@#|'<>.-^*()%!]{8,}", password4));
+
+        String[] validEmailSample = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
+        String[] invalidEmailSample = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+
+        System.out.println("Valid email :");
+        emailValidate(validEmailSample);
+        System.out.println("\nInvalid email :");
+        emailValidate(invalidEmailSample);
+
+    }
+
+    public static void emailValidate(String[] emails) {
+        for(int i = 0; i< emails.length; i++ ){
+            System.out.print(Pattern.matches("^[\\w+-]+(?:\\.[\\w+-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", emails[i]) + " ");
+        }
     }
 }
+
